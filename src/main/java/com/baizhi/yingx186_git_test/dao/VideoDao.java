caa69c2ae468133d4bbb7a005fbe2d2128e6827f
Duan Ya <1584508479@qq.com>
2020-04-14 23:13:11 +0800

    小黄提交的video实体类

---
+++ src/main/java/com/baizhi/yingx186_git_test/dao/VideoDao.java
@@ -0,0 +1,9 @@
+package com.baizhi.yingx186_git_test.dao;
+
+import com.baizhi.yingx186_git_test.entity.Video;
+
+import java.util.List;
+
+public interface VideoDao {
+    List<Video> queryAll();
+}
