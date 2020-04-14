76acd8127e75ce1a18aeecf565a98cd92a05001f
Duan Ya <1584508479@qq.com>
2020-04-14 22:08:16 +0800

    这是应学项目的第一个版本 v1.1.0

---
+++ src/main/java/com/baizhi/yingx186_git_test/Yingx186GitTestApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.yingx186_git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yingx186GitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yingx186GitTestApplication.class, args);
+    }
+
+}
