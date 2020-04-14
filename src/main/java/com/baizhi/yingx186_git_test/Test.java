72a5b5c14d074d2c92a1f87dffb0ab637aa384be
Duan Ya <1584508479@qq.com>
2020-04-14 22:25:15 +0800

    这是应学项目的第一个版本 v1.1.4

--- src/main/java/com/baizhi/yingx186_git_test/Test.java
+++ src/main/java/com/baizhi/yingx186_git_test/Test.java
@@ -3,5 +3,6 @@ package com.baizhi.yingx186_git_test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("s");
+        System.out.println("--");
     }
 }
